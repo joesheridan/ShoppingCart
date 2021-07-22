30f0c0ce227819f12937820fa9f31c3508a1a12d
Joe Sheridan <joe.sheridan@gfsdeliver.com>
2021-07-22 16:03:22 +0100

    added discounts code

--- src/main/scala/com/shopping/cart/ShoppingCart.scala
+++ src/main/scala/com/shopping/cart/ShoppingCart.scala
@@ -15,11 +15,25 @@ class ShoppingCart {
     }
   }
 
+  private def get3for2OrangesDiscount(items: List[Item]): Double = {
+    val oranges = items.count(i => i == Orange)
+    (oranges / 3) * getValue(Orange)
+  }
+
+  private def getBuyOneGetOneFreeApplesDiscount(items: List[Item]): Double = {
+    val apples = items.count(i => i == Apple)
+    (apples / 2) * getValue(Apple)
+  }
+
   private def getTotal(items: List[Item]): Double = {
     items.map(getValue).reduce( _ + _)
   }
 
+  private def getDiscounts(items: List[Item]): Double = {
+    get3for2OrangesDiscount(items) + getBuyOneGetOneFreeApplesDiscount(items)
+  }
+
   def checkout(items: List[Item]): Double = {
-      getTotal(items)
+    getTotal(items) - getDiscounts(items)
   }
 }
--- src/test/scala/com/shopping/cart/ShoppingCartSpec.scala
+++ src/test/scala/com/shopping/cart/ShoppingCartSpec.scala
@@ -9,4 +9,19 @@ class ShoppingCartTest extends AnyFunSuite {
     val cart = new ShoppingCart()
     assert(cart.checkout(items) === 0.85)
   }
+
+  test("ShoppingCart.checkout.3for2Oranges") {
+    val items = List(Apple, Orange, Orange, Orange)
+    val cart = new ShoppingCart()
+    assert(cart.checkout(items) === 1.1)
+
+    val items2 = List(Apple, Orange, Orange, Orange, Orange, Orange, Orange, Orange)
+    assert(cart.checkout(items2) === 1.85)
+  }
+
+  test("ShoppingCart.checkout.BuyOneGetOneFreeApples") {
+    val items = List(Apple, Apple, Apple, Apple)
+    val cart = new ShoppingCart()
+    assert(cart.checkout(items) === 1.2)
+  }
 }
